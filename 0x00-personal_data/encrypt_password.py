158926eaae322176e203b1bb8692ab20c402234e
Matrix30 <Matrixboy30@gmail.com>
2024-05-17 18:48:19 +0100

    finish task 5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+contains functions related to encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes password to byte string using hashpw function of
+    bcrypt package
+    Args:
+                password(str): the password to hash
+        Returns:
+                bytes: salted hashed password
+    """
+    salt = bcrypt.gensalt()
+    password = password.encode("utf-8")
+    return bcrypt.hashpw(password, salt)
+
+
+def is_valid(has):
+    """
+    verify if a password corresponds to a hash value
+    """
+    pass
