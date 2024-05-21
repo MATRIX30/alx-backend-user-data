44ecc9f9f7049bc3738db687e900fb52c8e9232f
Matrix30 <Matrixboy30@gmail.com>
2024-05-21 12:45:57 +0100

    finish task 3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to handle authentication
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    class to handle API authentications
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        method to return required authentication
+        """
+        return False
+    
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        returns authorized headers
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns current user
+        """
