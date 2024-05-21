2ed6665d89874b08b1abc4068ee78ec77cda1045
Matrix30 <Matrixboy30@gmail.com>
2024-05-21 14:25:03 +0100

    finish task 6. Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
-from api.v1.auth.auth import Auth
 import os
 
 
@@ -15,9 +14,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = getenv("AUTH_TYPE")
-if auth:
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth:
     from api.v1.auth.auth import Auth
-
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to handle basic authentication
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    class to handle basic authentication
+    """
+    pass
