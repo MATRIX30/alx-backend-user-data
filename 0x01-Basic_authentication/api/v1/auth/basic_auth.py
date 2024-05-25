633d86a25886d7da9b8517d1221205347f154ebc
Matrix30 <Matrixboy30@gmail.com>
2024-05-25 10:09:00 +0100

    Revert "finish task 8. Basic - Base64 decode"
    This reverts commit 9fcc6a26a12a636adb13d1866b7fc1669457eb00.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,11 +4,6 @@
 
 
 from api.v1.auth.auth import Auth
-from typing import Tuple, TypeVar
-import binascii
-import base64
-import re
-from models.user import User
 
 
 class BasicAuth(Auth):
@@ -28,78 +23,3 @@ class BasicAuth(Auth):
         if not (authorization_header.startswith("Basic ")):
             return None
         return authorization_header.split(" ")[1]
-
-    def decode_base64_authorization_header(
-        self, base64_authorization_header: str
-    ) -> str:
-        """method to decode base64 encoding"""
-        if base64_authorization_header is None:
-            return None
-        if type(base64_authorization_header) != str:
-            return None
-        try:
-            decoded_base64_header = base64.b64decode(
-                base64_authorization_header)
-            decoded_str_msg = decoded_base64_header.decode("utf-8")
-            return decoded_str_msg
-        except Exception:
-            return None
-
-    def extract_user_credentials(
-            self,
-            decoded_base64_authorization_header: str,
-            ) -> Tuple[str, str]:
-        """Extracts user credentials from a base64-decoded authorization
-        header that uses the Basic authentication flow.
-        """
-        if type(decoded_base64_authorization_header) == str:
-            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
-            field_match = re.fullmatch(
-                pattern,
-                decoded_base64_authorization_header.strip(),
-            )
-            if field_match is not None:
-                user = field_match.group('user')
-                password = field_match.group('password')
-                return user, password
-        return None, None
-
-    def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """Returns the User instance based on the email and password.
-
-        Args:
-            user_email (str): The user's email.
-            user_pwd (str): The user's password.
-
-        Returns:
-            User: The User instance or None if the user is not found or the
-            password is invalid.
-        """
-        # Return None if user_email or user_pwd is None or not a string
-        if not all(map(lambda x: isinstance(x, str), (user_email, user_pwd))):
-            return None
-        try:
-            # Search for the user in the database
-            user = User.search(attributes={'email': user_email})
-        except Exception:
-            return None
-        # Return None if there is no user in the database with the given email
-        if not user:
-            return None
-        # Get the first user from the search results
-        user = user[0]
-        # Return None if the password is invalid
-        if not user.is_valid_password(user_pwd):
-            return None
-        # Return the user instance
-        return user
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieves the user from a request.
-        """
-        auth_header = self.authorization_header(request)
-        b64_auth_token = self.extract_base64_authorization_header(auth_header)
-        auth_token = self.decode_base64_authorization_header(b64_auth_token)
-        email, password = self.extract_user_credentials(auth_token)
-        return self.user_object_from_credentials(email, password)
