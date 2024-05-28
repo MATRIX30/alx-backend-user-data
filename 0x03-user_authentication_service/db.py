be39312f8148002d81f18a724065b6a0f3b091e4
Matrix30 <Matrixboy30@gmail.com>
2024-05-28 16:29:00 +0100

    finish task 1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password) -> TypeVar('User'):
         """
         Adds a new user to the database.
         Args:
