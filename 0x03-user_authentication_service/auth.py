3a3e210ad44a43ea2c6341cbbeca5dd828f725ee
Matrix30 <Matrixboy30@gmail.com>
2024-05-30 09:14:50 +0100

    finish task 5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,7 +48,7 @@ class Auth:
             return None
 
         try:
-            search_user = self._db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
