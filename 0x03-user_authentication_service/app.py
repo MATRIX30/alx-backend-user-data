21a7fb6855890c2a90f11f44be6457385d05f2c8
Matrix30 <Matrixboy30@gmail.com>
2024-06-01 00:29:50 +0100

    finish task 7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,11 @@ main authentication flask service
 """
 
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +16,22 @@ app = Flask(__name__)
 def index():
     """home route"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", strict_slashes=False, methods=["POST"])
+def users():
+    """route function to handle users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or password is None:
+        return
+    if type(email) != str or type(password) != str:
+        return
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
