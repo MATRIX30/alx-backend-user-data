96b28b8f307b498b931eead6a920c5d3cec6ec69
Matrix30 <Matrixboy30@gmail.com>
2024-05-31 17:03:50 +0100

    finish task 4. Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    hashes a string password
+    Args:
+        password(str): string password to be hashed
+    Returns:
+        hashed_password(bytes): the hashed password hashed_password
+    """
+    if type(password) != str:
+        return None
+    encoded_password = password.encode("utf-8")
+    salt = bcrypt.gensalt(encoded_password)
+    hashed_password = bcrypt.hashpw(encoded_password, salt)
+    return hashed_password
