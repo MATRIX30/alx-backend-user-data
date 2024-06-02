b7f663503261ac890fa6d13bdc5a055d48811a84
Matrix30 <Matrixboy30@gmail.com>
2024-06-02 23:43:39 +0100

    fix saving sessionid to db

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,8 +95,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
-            user_id = user.id
-            self._db.update_user(user_id, session_id=session_id)
+            user.session_id = session_id
             return session_id
         except Exception:
             return None
