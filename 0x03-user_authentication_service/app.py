9d6cac7b0d3c6fe39d686b45f699f47e9326953c
Matrix30 <Matrixboy30@gmail.com>
2024-06-03 01:45:30 +0100

    Finish task  11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 main authentication flask service
 """
 
-from flask import Flask
+from flask import Flask, abort, make_response
 from flask import jsonify, request
 from auth import Auth
 
@@ -32,6 +32,21 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", strict_slashes=False, methods=["POST"])
+def login():
+    """
+    method to handle login
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
